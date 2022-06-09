92b31dab53bcc8ae6930e71907046fe70c351611
evgeniy <evgeni@digitalbutlers.team>
2022-06-09 18:05:59 +0300

    fix styles for news section for safari

--- src/scripts/mixins/swipper-news.js
+++ src/scripts/mixins/swipper-news.js
@@ -13,15 +13,15 @@ swiperNews.append(newsBtnPagination);
 // eslint-disable-next-line no-undef
 const swiper = new Swiper('.swiper.news-bottom', {
 	speed: 700,
-	autoHeight: true,
+	/* autoHeight: true, */
 	simulateTouch: true,
 	// How many slides
 	slidesPerView: '1',
 	spaceBetween: 30,
-	/* autoplay:{
-          delay:5000,
-          disableOnInteraction: false,
-      }, */
+	/* autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	}, */
 	// infinite
 	loop: true,
 	loopedSlides: 4,
@@ -36,7 +36,29 @@ const swiper = new Swiper('.swiper.news-bottom', {
 		prevEl: '.swiper-button-prev.desktop-btn_prev',
 		nextEl: '.swiper-button-next.desktop-btn_next',
 	},
+	on: {
+		init() {
+			const swiperNewsWrapper = document.querySelector('.news-list.swiper-wrapper');
+			const activeSlideHeight = document.querySelector('.swiper-slide-active.partner-slider_wrp').scrollHeight;
+			swiperNewsWrapper.style.height = `${activeSlideHeight}px !important`;
+		},
+	},
 });
-swiper.updateAutoHeight(700);
 
-/* swiper-news */
+
+swiper.updateAutoHeight(700);
+const swiperNewsWrapper2 = document.querySelector('.news-list.swiper-wrapper');
+swiperNewsWrapper2.removeAttribute('style');
+swiper.on('afterInit', () => {
+	const swiperNewsWrapper = document.querySelector('.news-list.swiper-wrapper');	
+	const activeSlideHeight = document.querySelector('.swiper-slide-active.partner-slider_wrp').scrollHeight;
+	swiperNewsWrapper.style.height = activeSlideHeight + "px";
+});
+swiper.on('beforeTransitionStart', () => {
+	const swiperNewsWrapper = document.querySelector('.news-list.swiper-wrapper');
+	const activeSlideHeight = document.querySelector('.swiper-slide-active.partner-slider_wrp').scrollHeight;
+	swiperNewsWrapper.style.height = activeSlideHeight +'px';
+});
+/* swiper.slideNext();
+swiper.updateAutoHeight(100); */
+
